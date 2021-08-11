0de0d908f38b43c50112622de48021675fa92376
Laguna1 <lagunazoomarket@gmail.com>
2021-08-12 01:38:11 +0300

    Add status column to comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
   validates :body, presence: true, length: { minimum: 10 }
 
-  has_many :comments
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
