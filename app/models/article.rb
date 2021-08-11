cdcef5c144e0c8683407dc41a15f1cf0c562c439
Laguna1 <lagunazoomarket@gmail.com>
2021-08-12 00:51:28 +0300

    Add associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20}
   validates :body, presence: true, length: { minimum: 10 }
+
+  has_many :comments
 end
