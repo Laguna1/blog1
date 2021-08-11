bc6aac30eb14d19316d0527a7b3de25989bbec66
Laguna1 <lagunazoomarket@gmail.com>
2021-08-12 01:49:21 +0300

    Add status field to articles and comments  forms

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  include Visible
   has_many :comments
 
   validates :title, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
